5948ef2a7987f4fa88b512a5f56dc456711b74f3
Angel <32056957+angelsol299@users.noreply.github.com>
2023-02-13 11:30:02 +0100

    Renamed To MainView

--- githubTest/ContentView.swift
+++ githubTest/ContentView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MainView: View {
      //Body comment
     
     var body: some View {
@@ -23,6 +23,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MainView()
     }
 }
