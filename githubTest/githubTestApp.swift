20363963f020edd3f315522939b70ded5a3fec59
Angel <32056957+angelsol299@users.noreply.github.com>
2023-02-13 11:26:33 +0100

    Initial Commit

---
+++ githubTest/githubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubTestApp.swift
+//  githubTest
+//
+//  Created by Angel Osoria on 2023-02-13.
+//
+
+import SwiftUI
+
+@main
+struct githubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
